dcd1fb7711246aa445672b38d35c4329b2f013ee
xiaojunjun <xiaojunjun@MacBookAir.(none)>
2025-10-30 18:00:22 +0800

    Sync: apply best-practices changes and remove build artifacts from index

--- run_hw1.ps1
+++
@@ -1,3 +0,0 @@
-$scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
-Start-Process -FilePath "powershell" -ArgumentList "-NoExit","-Command","& '$scriptDir\dist\HW1_server.exe'"
-Start-Process -FilePath "powershell" -ArgumentList "-NoExit","-Command","& '$scriptDir\dist\HW1_client.exe'"
